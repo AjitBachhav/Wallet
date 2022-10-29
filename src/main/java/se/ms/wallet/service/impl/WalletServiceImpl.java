022b1c5601cf0b74ae28704940c5c6ad2b60cdc5
Conny Backas <backasconny@gmail.com>
2022-10-29 21:26:36 +0200

    Implemented performTransaction

--- src/main/java/se/ms/wallet/service/impl/WalletServiceImpl.java
+++ src/main/java/se/ms/wallet/service/impl/WalletServiceImpl.java
@@ -1,6 +1,7 @@
 package se.ms.wallet.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 import se.ms.wallet.entity.Account;
 import se.ms.wallet.entity.Transaction;
 import se.ms.wallet.enums.TransactionType;
@@ -8,7 +9,11 @@ import se.ms.wallet.repository.AccountRepository;
 import se.ms.wallet.service.WalletService;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 public class WalletServiceImpl implements WalletService {
@@ -21,13 +26,27 @@ public class WalletServiceImpl implements WalletService {
 
     @Override
     public Transaction performTransaction(final TransactionType type, final Long playerId, final BigDecimal amount, final String transactionId) {
-        return null;
+        final Account account = accountRepository.findById(playerId)
+                .orElseThrow(() -> new HttpServerErrorException(NOT_FOUND, "Couldn't find an account by given id."));
+
+        final BigDecimal balance = account.getBalance();
+        final BigDecimal newBalance = balance.add(amount.multiply(BigDecimal.valueOf(type.getSignum())));
+
+        if (newBalance.compareTo(BigDecimal.ZERO) < 0) {
+            throw new HttpServerErrorException(FORBIDDEN, "Negative account balance is not allowed.");
+        }
+
+        final Transaction transaction = new Transaction(UUID.fromString(transactionId), amount, type, LocalDateTime.now());
+        account.setBalance(newBalance);
+        account.addTransaction(transaction);
+        accountRepository.save(account);
+        return transaction;
     }
 
     @Override
     public BigDecimal getWalletBalance(final Long playerId) {
         return accountRepository.findById(playerId).map(Account::getBalance)
-                .orElseThrow(() -> new HttpServerErrorException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new HttpServerErrorException(NOT_FOUND));
     }
 
     @Override
