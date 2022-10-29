791ed250293887c9a64379ac24999ef0a58de569
Conny Backas <backasconny@gmail.com>
2022-10-29 19:59:43 +0200

    Implemented controllers

---
+++ src/main/java/se/ms/wallet/controller/PlayerController.java
@@ -0,0 +1,26 @@
+package se.ms.wallet.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.ms.wallet.entity.Player;
+import se.ms.wallet.service.PlayerService;
+
+@RestController
+@RequestMapping("/api/player")
+public class PlayerController {
+
+    private PlayerService playerService;
+
+    public PlayerController(final PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @PostMapping(path = "/{name}")
+    public ResponseEntity<String> createPlayer(@PathVariable final String name) {
+        final Player player = playerService.createPlayer(name);
+        return ResponseEntity.ok("Player " + player.getName() + " created.");
+    }
+}
---
+++ src/main/java/se/ms/wallet/controller/WalletController.java
@@ -0,0 +1,48 @@
+package se.ms.wallet.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.ms.wallet.entity.Transaction;
+import se.ms.wallet.enums.TransactionType;
+import se.ms.wallet.service.WalletService;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/wallet")
+public class WalletController {
+
+    private WalletService walletService;
+
+    public WalletController(final WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PutMapping(path = "/transaction/{type}/{playerId}/{amount}/{transactionId}")
+    public ResponseEntity<Transaction> performTransaction(
+            @PathVariable final TransactionType type,
+            @PathVariable final Long playerId,
+            @PathVariable final BigDecimal amount,
+            @PathVariable final String transactionId) {
+
+        return ResponseEntity.ok(walletService.performTransaction(type, playerId, amount, transactionId));
+    }
+
+    @GetMapping(path = "/balance/{playerId}")
+    public ResponseEntity<BigDecimal> getWalletBalance(@PathVariable final Long playerId) {
+        final BigDecimal walletBalance = walletService.getWalletBalance(playerId);
+        return walletBalance != null ?
+                ResponseEntity.ok(walletBalance) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @GetMapping(path = "/generateTransactionId")
+    public ResponseEntity<UUID> generateTransactionId() {
+        return ResponseEntity.ok(walletService.generateTransactionId());
+    }
+}
